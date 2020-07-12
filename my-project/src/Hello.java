3b5ef4548afaec25671d6b80b06492888c01c3d8
tomasz.lis <S@m8un6$>
2020-07-12 12:47:33 +0200

    Mój pierwszy program

---
+++ my-project/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Witaj w programie!");
+        System.out.println("Witaj w programie!");
+        System.out.print("Witaj w programie!");
+        System.out.print("Witaj w programie!");
+        String name = "Tomasz";
+        String surname = "Lis";
+        System.out.println(name + " " + surname);
+        System.out.println(name + " " + surname);
+        System.out.println("Koniec!");
+    }
+}
